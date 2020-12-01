ad4af178287ba212c7c7cd6fdc865d2b6aa20064
trev155 <trevku155@gmail.com>
2020-11-30 22:49:57 -0500

    linking to a forked node module (not committing it)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import { HexGrid, Layout, Hexagon, Text } from 'react-hexgrid';
+import ReactTooltip from 'react-tooltip'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <SkillGrid/>
+
+      
+
     </div>
   );
 }
@@ -31,7 +35,7 @@ class SkillGrid extends React.Component {
 
   render() {
     const gridData = this.state.gridData.map(node => {
-      const key = node.positionQ + "-" + node.positionR
+      const key = node.positionQ + "/" + node.positionR
       return (
         <Node key={key} node={node}/>
       );
@@ -39,11 +43,14 @@ class SkillGrid extends React.Component {
 
     return (
       <div className="skillGrid">
-        <HexGrid width={800} height={800} viewBox="-110 -110 220 220">
+        <HexGrid width={800} height={800} viewBox="-150 -110 220 220">
           <Layout size={{ x: 10, y: 10 }} flat={true} spacing={1.1} origin={{ x: 0, y: 0 }}>
             {gridData}
           </Layout>
         </HexGrid>
+        <ReactTooltip id='svgTooltip2'>
+          123  
+        </ReactTooltip>
       </div>
     );
   }
@@ -53,7 +60,8 @@ class Node extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activated: false
+      activated: false,
+      hovering: false
     };
     this.activateAction = this.activateAction.bind(this);
   }
@@ -66,9 +74,15 @@ class Node extends React.Component {
 
   render() {
     const node = this.props.node;
+    const tooltipId = node.positionQ + "." + node.positionR;
+
     return (
-      <Hexagon q={node.positionQ} r={node.positionR}>
-        <Text>{node.positionR}/{node.positionQ}</Text>
+      <Hexagon 
+        className={this.state.activated ? "activated" : ""}
+        onClick={this.activateAction}
+        data-tip='true' data-for='svgTooltip2'
+        q={parseInt(node.positionQ)} r={parseInt(node.positionR)} s={0}>
+        <Text>{node.displayTextShort}</Text>
       </Hexagon>
     );
   }
